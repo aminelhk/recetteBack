1e0cf79ae98eb3e278ea0133397ed3c34159741d
aminelhk <amine.professionnel1@gmail.com>
2023-12-20 15:46:33 +0100

    dernier push

--- controllers/recetteCuisine.js
+++ controllers/recetteCuisine.js
@@ -13,25 +13,45 @@ const addNewRecette = (req,res) => {
     })
 }
 
-const getAllRecettes = (req,res) => {
-    recette.find({},(err,answer) => {
-        if(err){res.send(err);}
-    else{
-        res.send(answer);
-}
-})
+// const addNewRecette = (req,res) => {
+//     let newRecette = new Recette(req.body);
+//     newRecette.save()
+//         .then(answer => res.send(answer))
+//         .catch(err => res.status(500).send(err));
+// }
+
+// const getAllRecettes = (req,res) => {
+//     recette.find({},(err,answer) => {
+//         if(err){res.send(err);}
+//     else{
+//         res.send(answer);
+// }
+// })
+// }
+
+const getAllRecettes = (req, res) => {
+    recette.find()
+        .then(answer => res.send(answer))
+        .catch(err => res.status(500).send(err));
 }
 
 const getOneRecetteById = (req, res) => {
     const recetteId = req.params.id;
-    recette.findOne({ _id: recetteId }, (err, answer) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(answer);
-        }
-    });
+    recette.findOne({ _id: recetteId })
+        .then(answer => res.send(answer))
+        .catch(err => res.status(500).send(err));
 }
+
+// const getOneRecetteById = (req, res) => {
+//     const recetteId = req.params.id;
+//     recette.findOne({ _id: recetteId }, (err, answer) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.send(answer);
+//         }
+//     });
+// }
 
 
 
--- routes/recetteCuisineRoute.js
+++ routes/recetteCuisineRoute.js
@@ -8,9 +8,12 @@ const {addNewRecette,getAllRecettes,getOneRecetteById} = require('../controllers
 
 //Create a new recipe
 //router.post('/recette', addNewRecette)
+router.get('/recette', getAllRecettes);
+router.get('/recetteDetail/:id', getOneRecetteById);
+
+// ATTENTION CETTE ROUTE CONTIENT DU CODE METIER, ELLE NE RESPECTE LE MVC
 router.post('/recette', async (req, res) => {
     const {name,ingredients,origine,instructions,lienImage} = req.body;
-
 
     try {
         const recette = new Recette ({name,ingredients,origine,instructions,lienImage});
@@ -22,26 +25,26 @@ router.post('/recette', async (req, res) => {
     }
 })
 
-router.get ('/recette', async(req, res) =>{
-    try {
-        const recettes = await Recette.find({});
-        res.send(recettes);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-});
+// router.get ('/recette', async(req, res) =>{
+//     try {
+//         const recettes = await Recette.find({});
+//         res.send(recettes);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send(error);
+//     }
+// });
 
-router.get ('/recetteDetail/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const recettes = await Recette.findById(id);
-        res.send(recettes);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-});
+// router.get ('/recetteDetail/:id', async(req, res) =>{
+//     try {
+//         const {id} = req.params;
+//         const recettes = await Recette.findById(id);
+//         res.send(recettes);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send(error);
+//     }
+// });
 
 module.exports = router;
 
