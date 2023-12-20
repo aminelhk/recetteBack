926b24c4331e89736fe636341fd6426546cb196e
Finlais <arnaud.le-cozannet@orange.fr>
2023-12-20 16:08:24 +0100

    feat

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes/recetteCuisineRoute');
-const bodyParser = require ('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes/recetteCuisineRoute");
+const bodyParser = require("body-parser");
 
-const dbUrl = "mongodb://localhost:27017/recette"
+const dbUrl = "mongodb://localhost:27017/recette";
 
-async function connectDB(){
-    try{
-        await mongoose.connect(dbUrl);
-        console.log('MongoDB successfully connected');
-    } catch(err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-
+async function connectDB() {
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("MongoDB successfully connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 }
 
 const app = express();
@@ -21,12 +21,11 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(8082, () => {
-    console.log("Serveur à l'écoute");
-
-})
-
+  console.log("Serveur à l'écoute");
+});
